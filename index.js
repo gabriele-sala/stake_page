a63c47082aca18c8e9187ac4ff8d0985643bf6df
gabriele-sala <83841730+gabriele-sala@users.noreply.github.com>
2024-02-09 02:52:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-require('dotenv').config(); // Load environment variables
+require('dotenv').config();
 const express = require('express');
 const Web3 = require('web3');
+const { calculateAPR } = require('./services/calculate-apr'); // Adjust the path as necessary
 const app = express();
 const port = 3000;
 
@@ -11,14 +12,10 @@ app.use(express.static('public')); // Serve static files
 
 app.get('/api/calculate-apr', async (req, res) => {
   try {
-    const staticAddress = '0x4ab6FFa52460979DdE1E442FB95F8BaC56C3AdC3'; // Example Ethereum address
-    const simulatedBalanceInWei = web3.utils.toWei('1.5', 'ether');
-    const balanceInEther = web3.utils.fromWei(simulatedBalanceInWei, 'ether');
-
-    const apr = (2500000 / parseFloat(balanceInEther)) / 43 * 365; // Example APR calculation
-    res.status(200).json({ apr: `${apr.toFixed(2)}%` });
+    const apr = await calculateAPR(web3); // Pass the web3 instance
+    res.status(200).json({ apr: `${apr}%` }); // Send APR as a string with a "%" sign
   } catch (error) {
-    console.error('Error calculating APR:', error);
+    console.error('API Error:', error);
     res.status(500).json({ error: 'Failed to calculate APR' });
   }
 });
