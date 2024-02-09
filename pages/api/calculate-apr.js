68b2da24e3539074e12fb31ab2883617c80ee297
gabriele-sala <83841730+gabriele-sala@users.noreply.github.com>
2024-02-09 01:55:55 +0100

    Update calculate-apr.js

--- pages/api/calculate-apr.js
+++ pages/api/calculate-apr.js
@@ -1,11 +1,11 @@
 const Web3 = require('web3');
-const infuraProjectId = 'YOUR_INFURA_PROJECT_ID'; // Replace with your Infura project ID
+const infuraProjectId = 'faf348e8e5554ff0a870792631b24807'; // Replace with your Infura project ID
 
 const web3 = new Web3('https://mainnet.infura.io/v3/' + infuraProjectId);
 
 export default async function handler(req, res) {
   try {
-    const staticAddress = '0x...'; // Replace with your static Ethereum address
+    const staticAddress = '0x4ab6FFa52460979DdE1E442FB95F8BaC56C3AdC3'; // Replace with your static Ethereum address
     const simulatedBalanceInWei = web3.utils.toWei('1.5', 'ether'); 
     const balanceInEther = web3.utils.fromWei(simulatedBalanceInWei, 'ether');
 
@@ -17,3 +17,4 @@ export default async function handler(req, res) {
     res.status(500).json({ error: 'Failed to calculate APR' });
   }
 }
+
